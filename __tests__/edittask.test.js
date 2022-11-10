047b4bdce46041322dcd53cac399307c47f91227
storres20 <sanchotorres777@gmail.com>
2022-11-10 18:15:14 -0500

    Fix edittask testing file

--- __tests__/edittask.test.js
+++ __tests__/edittask.test.js
@@ -14,6 +14,9 @@ describe('edittask', () => {
       </div>`
     + '</div>';
 
+    // clear localstorage
+    localStorage.setItem('tasks', JSON.stringify([]));
+
     // add a task in localstorage
     const task = {
       description: 'new task', completed: true, index: 3, id: '777',
@@ -37,5 +40,75 @@ describe('edittask', () => {
     // the task from localstorage was edited from "new task" to "new task edited"
     const getedit = JSON.parse(localStorage.getItem('tasks'));
     expect(getedit[0].description).toEqual('new task edited');
+  });
+
+  test('other case', () => {
+    document.body.innerHTML = '<div id="todoList">'
+    + `<div class="todoFlex">
+        <div class="todoDiv">
+          <input type="checkbox" name="777x" class="checkbox" checked >
+          <input class="todoP" name="777" value="new task" >
+        </div>
+        <i class="bi bi-three-dots-vertical dots"></i>
+      </div>`
+    + '</div>';
+
+    // clear localstorage
+    localStorage.setItem('tasks', JSON.stringify([]));
+
+    // add a task in localstorage
+    const task = {
+      description: 'new task 888', completed: true, index: 8, id: '888',
+    };
+    storeRetrieve(task);
+
+    // edittask function
+    edittask();
+
+    // Event
+    const inputEdit = document.querySelector('.todoP');
+    // Create a new 'change' event
+    const event = new Event('change', { bubbles: true });
+    // Dispatch it.
+    inputEdit.dispatchEvent(event);
+
+    // other case task
+    const getedit = JSON.parse(localStorage.getItem('tasks'));
+    expect(getedit[0].description).toEqual('new task 888');
+  });
+
+  test('when you dont edit a task', () => {
+    document.body.innerHTML = '<div id="todoList">'
+    + `<div class="todoFlex">
+        <div class="todoDiv">
+          <input type="checkbox" name="777x" class="checkbox" checked >
+          <input class="todoP" name="777" value="new task" >
+        </div>
+        <i class="bi bi-three-dots-vertical dots"></i>
+      </div>`
+    + '</div>';
+
+    // clear localstorage
+    localStorage.setItem('tasks', JSON.stringify([]));
+
+    // add a task in localstorage
+    const task = {
+      description: 'new task 888', completed: true, index: 8, id: '888',
+    };
+    storeRetrieve(task);
+
+    // edittask function
+    edittask();
+
+    // Event
+    const checkbox = document.querySelector('.checkbox');
+    // Create a new 'change' event
+    const event = new Event('change', { bubbles: true });
+    // Dispatch it.
+    checkbox.dispatchEvent(event);
+
+    // when you dont edit a task
+    const getedit = JSON.parse(localStorage.getItem('tasks'));
+    expect(getedit[0].description).toEqual('new task 888');
   });
 });
