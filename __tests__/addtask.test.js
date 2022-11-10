258a3500a56864641a370aec291d2a3ec4d2b72c
Giorgi Bokuchava <g.bokuchava22@gmail.com>
2022-11-10 08:31:17 -0800

    Add testing file for addtask function

---
+++ __tests__/addtask.test.js
@@ -0,0 +1,40 @@
+/* @jest-environment jsdom */
+const addtask = require('../modules/addtask');
+const storeRetrieve = require('../modules/storeRetrieve')
+
+describe('addtask', ()=>{
+    test('Add one new task to the list when completed is false', () =>{
+        document.body.innerHTML =
+        '<div id="todoList">' +
+        '</div>';
+        
+        const task = {description: 'new task', completed: false, index: 0, id: 123}
+    
+        addtask(task);
+        const list = document.querySelectorAll('.todoFlex');
+        expect(list).toHaveLength(1);
+    })
+
+    test('Add one new task to the list when completed is true', () =>{
+        document.body.innerHTML =
+        '<div id="todoList">' +
+        '</div>';
+        
+        const task = {description: 'new task 02', completed: true, index: 1, id: 456}
+    
+        addtask(task);
+        const list = document.querySelectorAll('.todoFlex');
+        expect(list).toHaveLength(1);
+    })
+
+    test('Add one new task to localStorage', () =>{
+        
+        const task = {description: 'new task 03', completed: false, index: 2, id: 789}
+    
+        storeRetrieve(task);
+        const taskLocal = JSON.parse(localStorage.getItem('tasks'))
+        expect(taskLocal).toHaveLength(3);
+    })
+
+
+})
